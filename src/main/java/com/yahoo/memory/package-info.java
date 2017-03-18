f0dcd644580520ee6ca2e4b04cf7f71be4025727
lrhodes <lrhodes@yahoo-inc.com>
2017-03-17 18:53:35 -0700

    Update javadocs

--- src/main/java/com/yahoo/memory/package-info.java
+++ src/main/java/com/yahoo/memory/package-info.java
@@ -1,6 +1,6 @@
 /**
  * The goal of this package is to provide high performance access to native memory for primitives
- * and primitive arrays. Also provides compatible views into heap-based arrays and the
+ * and primitive arrays. It also provides compatible views into heap-based arrays and the
  * Java ByteBuffer class. This package is general purpose, has no external dependencies and can
  * be used in any application that needs to manage data structures outside the Java heap.
  *

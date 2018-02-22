511bdc075a1dcb91cde6fbeb936d95b1d79c4728
lrhodes <lrhodes@yahoo-inc.com>
2018-02-22 15:04:47 -0800

    Changed to overflow-aware comparison.

--- src/main/java/com/yahoo/memory/CompareAndCopy.java
+++ src/main/java/com/yahoo/memory/CompareAndCopy.java
@@ -97,7 +97,7 @@ final class CompareAndCopy {
    */
   private static void copyMemoryOverlapAddressCheck(final Object srcUnsafeObj, final long srcAdd,
       final Object dstUnsafeObj, final long dstAdd, final long lengthBytes) {
-    if (((srcAdd + lengthBytes) <= dstAdd) || ((dstAdd + lengthBytes) <= srcAdd)) {
+    if ((((srcAdd + lengthBytes) - dstAdd) <= 0) || (((dstAdd + lengthBytes) - srcAdd) <= 0)) {
       copyNonOverlappingMemoryWithChunking(srcUnsafeObj, srcAdd, dstUnsafeObj, dstAdd,
           lengthBytes);
       return;

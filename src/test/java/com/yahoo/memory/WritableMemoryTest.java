db7c15d340d31c51f91469c862185133b41d6f69
lrhodes <lrhodes@yahoo-inc.com>
2018-05-06 17:11:08 -0700

    Added FindBugs dependency, scope=compile to Pom.
    Now we can use SuppressFBWarnings(...)
    Allow "unhandled SuppressWarnings" tokens.

--- src/test/java/com/yahoo/memory/WritableMemoryTest.java
+++ src/test/java/com/yahoo/memory/WritableMemoryTest.java
@@ -50,11 +50,13 @@ public class WritableMemoryTest {
     wmem.getByteArray(0, srcAndDst, 64, 64);  //non-overlapping
   }
 
+  @SuppressWarnings({"EqualsWithItself", "SelfEquals"})
+  //SelfEquals for Plexus, EqualsWithItself for IntelliJ
   @Test
   public void checkEquals() {
     int len = 7;
     WritableMemory wmem1 = WritableMemory.allocate(len);
-    //assertTrue(wmem1.equals(wmem1)); //intentionally ignoring this check
+    assertTrue(wmem1.equals(wmem1));
 
     WritableMemory wmem2 = WritableMemory.allocate(len + 1);
     assertFalse(wmem1.equals(wmem2));

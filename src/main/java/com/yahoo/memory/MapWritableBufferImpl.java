e1c6305b0061965a1de61256401463441b815be2
lrhodes <lrhodes@yahoo-inc.com>
2018-06-13 17:41:53 -0700

    Add map leafs

---
+++ src/main/java/com/yahoo/memory/MapWritableBufferImpl.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018, Yahoo! Inc. Licensed under the terms of the
+ * Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+
+package com.yahoo.memory;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * Implementation of {@link WritableBuffer} for map memory, native byte order.
+ *
+ * @author Roman Leventov
+ * @author Lee Rhodes
+ */
+class MapWritableBufferImpl extends WritableBufferImpl {
+  private final long nativeBaseOffset;
+  final StepBoolean valid; //a reference only
+
+  MapWritableBufferImpl(
+      final long nativeBaseOffset,
+      final long regionOffset,
+      final long capacityBytes,
+      final boolean readOnly,
+      final StepBoolean valid,
+      final BaseWritableMemoryImpl originMemory) {
+    super(null, nativeBaseOffset, regionOffset, capacityBytes, readOnly, null, valid, originMemory);
+    this.nativeBaseOffset = nativeBaseOffset;
+    this.valid = valid;
+    if (valid == null) {
+      throw new IllegalArgumentException("Valid cannot be null.");
+    }
+  }
+
+  @Override
+  public ByteBuffer getByteBuffer() {
+    return null;
+  }
+
+  @Override
+  public ByteOrder getByteOrder() {
+    return Util.nativeOrder;
+  }
+
+  @Override //TODO remove from baseWMemImpl NOTE WRITABLE ONLY
+  public MemoryRequestServer getMemoryRequestServer() {
+    return null;
+  }
+
+  @Override
+  long getNativeBaseOffset() {
+    return nativeBaseOffset;
+  }
+
+  @Override
+  Object getUnsafeObject() {
+    return null;
+  }
+
+  @Override
+  StepBoolean getValid() {
+    return valid;
+  }
+
+  @Override
+  public boolean isValid() {
+    return valid.get();
+  }
+
+  @Override //TODO make abstract in BaseWritable?
+  void setMemoryRequestServer(final MemoryRequestServer svr) {
+
+  }
+
+}

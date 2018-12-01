020354d50109a4786f6823beed6dab90c25a1cd4
lrhodes <lrhodes@yahoo-inc.com>
2018-11-30 17:20:38 -0800

    Update hashCode computation

--- src/test/java/com/yahoo/memory/MemoryTest.java
+++ src/test/java/com/yahoo/memory/MemoryTest.java
@@ -412,7 +412,7 @@ public class MemoryTest {
   public void checkHashCode() {
     WritableMemory wmem = WritableMemory.allocate(32 + 7);
     int hc = wmem.hashCode();
-    assertEquals(hc, 28629151);
+    assertEquals(hc, -960627348);
   }
 
   @Test
